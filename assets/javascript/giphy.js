9bf367c5bd4b637cc9cdcf0b9ede042a6258d3c4
Mitch Vassar <mitchvassarjr2@gmail.com>
2017-12-05 08:09:45 -0500

    added more to javascript files and attempted to make gifs start/stop(not working)

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,32 +1,51 @@
-$("button").on("click", function() {
+$( document ).ready(function() {
 
-    var person = $(this).attr("data-person");
-    var userInput = ;
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        person + "&api_key=dc6zaTOxFJmzC&limit=10&tag=" + userInput;
+  $("#addGif").on("click", function(noReload) {
+      noReload.preventDefault() //Prevents screen from reloading
+      //variables
+      var themeArray = [];
+      var gif = $(this).attr("gif-data");
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          gif + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-    $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
+          //AJAX call to get api data
+      $.ajax({
+              url: queryURL,
+              method: "GET"
+          }).done(function(response) {
+              var results = response.data;
+              console.log(results)
 
-        .done(function(response) {
-            var results = response.data;
+              //for loop to iterate through array and add images and classes/attributes to them
+              for (var i = 0; i < results.length; i++) {
+                  if (results[i].rating) {
 
-            for (var i = 0; i < results.length; i++) {
+                      var gifDiv = $("<div class='item'>");
+                      var rating = results[i].rating;
+                      var p = $("<p>").text("Rating: " + rating);
+                      var gifImg = $("<img>");
 
-                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-
-                    var gifDiv = $("<div class='item'>");
-                    var rating = results[i].rating;
-                    var p = $("<p>").text("Rating: " + rating);
-                    var personImage = $("<img>");
-
-                    personImage.attr("src", results[i].images.fixed_height.url);
-                    gifDiv.append(p);
-                    gifDiv.append(personImage);
-                    $("#gifs-appear-here").prepend(gifDiv);
-                }
-            }
-        });
+                      gifImg.addClass("giphy")
+                      gifImg.addClass("playing")
+                      gifImg.attr("src", results[i].images.fixed_height.url);
+                      gifDiv.append(p);
+                      gifDiv.append(gifImg);
+                      $("#gifs").prepend(gifDiv);
+                  }
+              }
+          });
+  });
+  //stops/starts gifs(doesn't work :( )
+  $(".giphy").on("click", function() {
+      var src = $(this).attr("src");
+    if($(this).hasClass("playing")){
+       //stop
+       $(this).attr("src", src.replace(/\.gif/i, "_s.gif"))
+       $(this).removeClass("playing");
+    } else {
+      //play
+      $(this).addClass("playing");
+      $(this).attr("src", src.replace(/\_s.gif/i, ".gif"))
+    }
+  });
 });
